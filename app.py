4904f4d99c34ab74648b6f499a455ccc204aa053
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-02-27 12:27:54 -0300

    atualizações

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import urllib.request
 import json
 import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context #Para evitar errores de certificado SSL
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -48,6 +50,57 @@ def get_list_elements():
         return {"characters": characters_list}
     except Exception as e:
         return {"error": str(e)}
+    
+@app.route('/locations')
+def get_locations():
+    url = "https://rickandmortyapi.com/api/location"
+    response = urllib.request.urlopen(url)
+    data = response.read().decode('utf-8')
+    locations_data = json.loads(data)
+    locations = locations_data['results']
+    return render_template("locations.html", locations=locations)
+
+@app.route('/location/<id>')
+def get_location(id):
+    url = "https://rickandmortyapi.com/api/location/"+id
+    context = ssl._create_unverified_context()
+    response = urllib.request.urlopen(url, context=context)
+    data = response.read()
+    location_dict = json.loads(data)
+
+    
+    characters_data = []
+    for resident_url in location_dict['residents']:
+        with urllib.request.urlopen(resident_url, context=context) as response:
+            character_data = json.loads(response.read())
+            characters_data.append(character_data)
+
+    return render_template("location.html", location=location_dict, characters=characters_data)
+
+@app.route('/episodes')
+def get_episodes():
+    url = "https://rickandmortyapi.com/api/episode"
+    response = urllib.request.urlopen(url)
+    data = response.read().decode('utf-8')
+    episodes_data = json.loads(data)
+    episodes = episodes_data['results']
+    return render_template("episodes.html", episodes=episodes)
+
+@app.route('/episode/<id>')
+def get_episode(id):
+    url = "https://rickandmortyapi.com/api/episode/"+id
+    context = ssl._create_unverified_context()
+    response = urllib.request.urlopen(url, context=context)
+    data = response.read()
+    episode_dict = json.loads(data)
+
+    characters_data = []
+    for character_url in episode_dict['characters']:
+        with urllib.request.urlopen(character_url, context=context) as response:
+            character_data = json.loads(response.read())
+            characters_data.append(character_data)
+
+    return render_template("episode.html", episode=episode_dict, characters=characters_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- historico.md
+++ historico.md
@@ -1,3 +1,15 @@
+*** A documentação da API Rick & Morty pode ser encontrada em: ***
+
+https://rickandmortyapi.com/documentation/#rest
+
+*** A documentação do Flask pode ser encontrada em: ***
+https://flask.palletsprojects.com/en/1.1.x/
+
+*** A documentação do Python pode ser encontrada em: ***
+https://docs.python.org/3/
+
+---------------------------------------------------------------------------
+
 python3 -m venv .venv
 
 .venv\Scripts\activate (no adm powershell)
@@ -10,5 +22,9 @@ pip install flask
 No terminal para rodar o servidor, digite o seguinte comando:
 
 flask --app app run
+
+ou
+
+python app.py
 
 Running on http://127.0.0.1:5000
